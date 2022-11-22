02f6f6768acca28c7219e5d1f15a0bbb171a0828
IvanRomero03 <i.wells.ar@gmail.com>
2022-11-22 02:13:54 -0600

    1_3 corregido

--- Act1_3/Registro.h
+++ Act1_3/Registro.h
@@ -6,6 +6,8 @@
 #include <vector>
 #include <ctime>
 #include <unordered_map>
+#include <fstream>
+#include <sstream>
 
 class Registro
 {
@@ -29,7 +31,9 @@ private:
 
 public:
     Registro();
+    Registro(std::string line);
     Registro(std::string mes, std::string dia, std::string hora, std::string minuto, std::string segundo, std::string ip, std::string puerto, std::string falla);
+    ~Registro();
     bool operator<(const Registro &r) const;
     bool operator<(const time_t &r) const;
     bool operator>(const Registro &r) const;
@@ -46,10 +50,12 @@ public:
     void print();
     std::string toString();
     friend std::ostream &operator<<(std::ostream &os, const Registro &r);
+    void readInput(std::string line);
 };
 
 Registro::Registro()
 {
+    // O(1)
     this->mes = "0";
     this->dia = "0";
     this->hora = "0";
@@ -61,8 +67,28 @@ Registro::Registro()
     this->timeStamp = 0;
 }
 
+Registro::Registro(std::string line)
+{
+    std::stringstream ss(line);
+    ss >> mes >> dia >> hora >> ip;
+    minuto = hora.substr(3, 2);
+    segundo = hora.substr(6, 2);
+    hora = hora.substr(0, 2);
+    puerto = ip.substr(ip.find(":") + 1, ip.size());
+    ip = ip.substr(0, ip.find(":"));
+    getline(ss, falla);
+    this->timeStruct.tm_mon = meses[mes];
+    this->timeStruct.tm_mday = stoi(dia);
+    this->timeStruct.tm_hour = stoi(hora);
+    this->timeStruct.tm_min = stoi(minuto);
+    this->timeStruct.tm_sec = stoi(segundo);
+    this->timeStruct.tm_year = 122;
+    this->timeStamp = mktime(&timeStruct);
+}
+
 Registro::Registro(std::string mes, std::string dia, std::string hora, std::string minuto, std::string segundo, std::string ip, std::string puerto, std::string falla)
 {
+    // O(1)
     this->mes = mes;
     this->dia = dia;
     this->hora = hora;
@@ -80,84 +106,89 @@ Registro::Registro(std::string mes, std::string dia, std::string hora, std::stri
     this->timeStamp = mktime(&timeStruct);
 }
 
+Registro::~Registro()
+{
+    // O(1)
+}
+
 bool Registro::operator==(const Registro &otro) const
 {
-    //O(1)
+    // O(1)
     return this->timeStamp == otro.timeStamp;
 }
 bool Registro::operator==(const time_t &otro) const
 {
-    //O(1)
+    // O(1)
     return this->timeStamp == otro;
 }
 bool Registro::operator!=(const Registro &otro) const
 {
-    //O(1)
+    // O(1)
     return !(*this == otro);
 }
 bool Registro::operator!=(const time_t &otro) const
 {
-    //O(1)
+    // O(1)
     return !(*this == otro);
 }
 bool Registro::operator<(const Registro &otro) const
 {
-    //O(1)
+    // O(1)
     return this->timeStamp < otro.timeStamp;
 }
 bool Registro::operator<(const time_t &otro) const
 {
-    //O(1)
+    // O(1)
     return this->timeStamp < otro;
 }
 bool Registro::operator>(const Registro &otro) const
 {
-    //O(1)
+    // O(1)
     return this->timeStamp > otro.timeStamp;
 }
 bool Registro::operator>(const time_t &otro) const
 {
-    //O(1)
+    // O(1)
     return this->timeStamp > otro;
 }
 bool Registro::operator<=(const Registro &otro) const
 {
-    //O(1)
+    // O(1)
     return this->timeStamp <= otro.timeStamp;
 }
 bool Registro::operator<=(const time_t &otro) const
 {
-    //O(1)
+    // O(1)
     return this->timeStamp <= otro;
 }
 bool Registro::operator>=(const Registro &otro) const
 {
-    //O(1)
+    // O(1)
     return this->timeStamp >= otro.timeStamp;
 }
 bool Registro::operator>=(const time_t &otro) const
 {
-    //O(1)
+    // O(1)
     return this->timeStamp >= otro;
 }
 time_t Registro::getTimeStamp()
 {
-    //O(1)
+    // O(1)
     return this->timeStamp;
 }
 void Registro::print()
 {
-    //O(1)
+    // O(1)
     std::cout << this->mes << " " << this->dia << " " << this->hora << ":" << this->minuto << ":" << this->segundo << " " << this->ip << ":" << this->puerto << " " << this->falla << std::endl;
 }
 std::string Registro::toString()
 {
-    //O(1)
+    // O(1)
     return this->mes + " " + this->dia + " " + this->hora + ":" + this->minuto + ":" + this->segundo + " " + this->ip + ":" + this->puerto + " " + this->falla;
 }
 std::ostream &operator<<(std::ostream &os, const Registro &r)
 {
-    //O(1)
+    // O(1)
     os << r.mes << " " << r.dia << " " << r.hora << ":" << r.minuto << ":" << r.segundo << " " << r.ip << ":" << r.puerto << " " << r.falla;
     return os;
 }
---
+++ mainAux.cpp
@@ -0,0 +1,176 @@
+/*
+    Iván Alberto Romero Wells - A00833623
+    Mariano Barberi Garza - A01571226
+
+    Compilación:
+        g++ -std=c++17 -Wall -o main *.cpp
+
+    Ejecución:
+        main
+*/
+#include <iostream>
+#include "Registro.h"
+#include <ctime>
+
+void readInput(std::vector<Registro> &Registros);
+void sort(std::vector<Registro> &Registros);
+int lowerBound(std::vector<Registro> &Registros, time_t fecha);
+int upperBound(std::vector<Registro> &Registros, time_t fecha);
+std::vector<Registro> getRango(std::vector<Registro> &Registros, time_t fechaInicio, time_t fechaFin);
+
+int main()
+{
+    std::vector<Registro> Registros;
+    std::string fecha1;
+    std::string mes, dia, hora, minuto, segundo;
+    std::cin >> mes >> mes;
+    std::cin >> mes >> dia >> hora;
+    minuto = hora.substr(3, 2);
+    segundo = hora.substr(6, 2);
+    hora = hora.substr(0, 2);
+    time_t first = Registro(mes, dia, hora, minuto, segundo, "", "", "").getTimeStamp();
+    std::cin >> mes >> mes;
+    std::cin >> mes >> dia >> hora;
+    minuto = hora.substr(3, 2);
+    segundo = hora.substr(6, 2);
+    hora = hora.substr(0, 2);
+    time_t last = Registro(mes, dia, hora, minuto, segundo, "", "", "").getTimeStamp();
+    readInput(Registros);
+    sort(Registros);
+
+    std::vector<Registro> rango = getRango(Registros, first, last);
+    if (rango[0] != first)
+    {
+        std::cout << "No se encontro el primer registro del rango" << std::endl;
+    }
+    if (rango[rango.size() - 1] != last)
+    {
+        std::cout << "No se encontro el ultimo registro del rango" << std::endl;
+    }
+    std::cout << std::endl;
+    for (auto i : rango)
+    {
+        std::cout << i << std::endl;
+    }
+
+    return 0;
+}
+
+void readInput(std::vector<Registro> &Registros)
+{
+    // O(n)
+    std::string line;
+    getline(std::cin, line);
+    int n, N;
+    std::string mes, dia, hora, minuto, segundo, falla, ip, puerto;
+    getline(std::cin, line);
+    getline(std::cin, line);
+    n = line.find(": ");
+    N = stoi(line.substr(n + 2, line.find(" ", n) - n - 2));
+    for (int i = 0; i < N; i++)
+    {
+        std::cin >> mes >> dia >> hora >> ip;
+        minuto = hora.substr(3, 2);
+        segundo = hora.substr(6, 2);
+        hora = hora.substr(0, 2);
+        puerto = ip.substr(ip.find(":") + 1, ip.size());
+        ip = ip.substr(0, ip.find(":"));
+        getline(std::cin, falla);
+        Registros.push_back(Registro(mes, dia, hora, minuto, segundo, ip, puerto, falla));
+    }
+    return;
+}
+
+void sort(std::vector<Registro> &Registros)
+{
+    // O(n log n)
+    std::vector<Registro> RegistrosSorted;
+    if (Registros.size() <= 1)
+    {
+        return;
+    }
+    else
+    {
+        std::vector<Registro> left(Registros.begin(), Registros.begin() + Registros.size() / 2);
+        std::vector<Registro> right(Registros.begin() + Registros.size() / 2, Registros.end());
+        sort(left);
+        sort(right);
+        int i = 0, j = 0, k = 0;
+        while (i < left.size() && j < right.size())
+        {
+            if (left[i].getTimeStamp() < right[j].getTimeStamp())
+            {
+                RegistrosSorted.push_back(left[i]);
+                i++;
+            }
+            else
+            {
+                RegistrosSorted.push_back(right[j]);
+                j++;
+            }
+        }
+        while (i < left.size())
+        {
+            RegistrosSorted.push_back(left[i]);
+            i++;
+        }
+        while (j < right.size())
+        {
+            RegistrosSorted.push_back(right[j]);
+            j++;
+        }
+        Registros = RegistrosSorted;
+    }
+    return;
+}
+
+int lowerBound(std::vector<Registro> &Registros, time_t fecha)
+{
+    // O(log n)
+    int i = 0, j = Registros.size() - 1, m;
+    while (i < j)
+    {
+        m = (i + j) / 2;
+        if (Registros[m].getTimeStamp() < fecha)
+        {
+            i = m + 1;
+        }
+        else
+        {
+            j = m;
+        }
+    }
+    return i;
+}
+
+int upperBound(std::vector<Registro> &Registros, time_t fecha)
+{
+    // O(log n)
+    int i = 0, j = Registros.size(), m;
+    while (i < j)
+    {
+        m = (i + j) / 2;
+        if (Registros[m].getTimeStamp() <= fecha)
+        {
+            i = m + 1;
+        }
+        else
+        {
+            j = m;
+        }
+    }
+    return i;
+}
+
+std::vector<Registro> getRango(std::vector<Registro> &Registros, time_t fechaInicio, time_t fechaFin)
+{
+    // O(log n)
+    std::vector<Registro> rango;
+    int i = lowerBound(Registros, fechaInicio);
+    int j = upperBound(Registros, fechaFin);
+    for (int k = i; k < j; k++)
+    {
+        rango.push_back(Registros[k]);
+    }
+    return rango;
+}
